af1b7a693f2e7a249c014899da753554c1c5b029
gabrielcaio11 <gabrielcaio848@gmail.com>
2026-03-28 16:52:37 -0300

    feat: implement MongoDB connection and add test endpoint

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    await mongoose.connection.db.admin().ping();
+
+    console.log('Ping no banco OK ✅');
+  } catch (error) {
+    console.error('Erro ao conectar no MongoDB:', error);
+    process.exit(1);
+  }
+};
